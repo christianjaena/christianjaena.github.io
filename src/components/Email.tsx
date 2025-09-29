b5e5568b3ac18a3eb393733ae6ed00ea645b1bcf
Christian Jaena <jaenachristian@gmail.com>
2025-09-29 22:12:55 +0800

    fixed alignment on social links

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -12,7 +12,7 @@ export const Email = () => {
         height="20"
         viewBox="0 0 24 24"
         fill="currentColor"
-        style={{ verticalAlign: "middle", marginRight: "6px" }}
+        style={{ verticalAlign: "middle" }}
         xmlns="http://www.w3.org/2000/svg"
       >
         <path d="M12 13.065l-11.99-8.065h23.98l-11.99 8.065zm-12-7.065v14h24v-14l-12 8.065-12-8.065z" />
